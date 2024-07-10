a16cb88d3de1a35b90b85585bad6a3326e73b888
mahesh-enugula <167786816+mahesh-enugula@users.noreply.github.com>
2024-07-11 01:14:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // Complete the js code
 function Car(make, model) {
-	this.make=make;
-	this.model=model;
+    this.make = make;
+    this.model = model;
 }
-Car prototype.getMakeModel=function () {
-		return`${this.make}, ${this.model}`;
+
+Car.prototype.getMakeModel = function () {
+    return `${this.make} ${this.model}`;
 };
 
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model);
-	this.topSpeed=topSpeed;
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
 
-SportsCar.prototype=Object.create(Car.prototype);
-SportsCar.prototype.constructor=SportsCar;
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
 
-SportsCar.prototype.getTopSpeed=function(){
-	return this.topSpeed;
-}
+SportsCar.prototype.getTopSpeed = function () {
+    return this.topSpeed;
+};
 
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 console.log(car.getMakeModel()); // Output: Ferrari Testarossa
